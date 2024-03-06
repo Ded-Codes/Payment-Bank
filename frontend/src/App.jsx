71a3a87a35265bcd15a855e9bd8c535586a51263
Ded-Codes <hraj44001@gmail.com>
2024-03-07 00:09:25 +0530

    Updated Code for User, Dashboard Component in Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route } from "react-router-dom"
 import { Signup } from "./pages/Signup"
+import { Signin } from "./pages/Signin"
 
 function App() {
 
@@ -8,8 +9,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/signup" element={<Signup/>}/>
-      {/* <Route path="/signin" element={<Signin/>}/>
-      <Route path="/dashboard"element={<Dashboard/>}/>
+      <Route path="/signin" element={<Signin/>}/>
+      {/* <Route path="/dashboard"element={<Dashboard/>}/>
       <Route path="/send" element={<SendMoney/>}/> */}
     </Routes>
     </BrowserRouter>
