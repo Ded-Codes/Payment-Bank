6b02878b07702b2745f5aece4bc28a306c0d1044
Ded-Codes <hraj44001@gmail.com>
2024-03-08 23:27:30 +0530

    Updated code for Signup Component with added post functionality using Axios

--- frontend/src/components/Inputbox.jsx
+++ frontend/src/components/Inputbox.jsx
@@ -1,8 +1,8 @@
-export function Inputbox({label, placeholder}){
+export function Inputbox({label, placeholder, onChange}){
     return(
         <div className="text-sm text-bold font-medium text-left py-2">
            {label}
-            <input placeholder={placeholder} className="w-full px-2 py-1 border-rounded border-slate-200"/>
+            <input onChange={onChange} placeholder={placeholder} className="w-full px-2 py-1 border-rounded border-slate-200"/>
         </div>
     )
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,22 +1,41 @@
+import { useState } from "react"
 import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
 import { Inputbox } from "../components/Inputbox"
 import { SubHeading } from "../components/SubHeading"
+import axios from "axios"
 
 export const Signup = () =>{
+    
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUserName] = useState("");
+    const [password, setpassword] = useState("");
     return (
         <div className="bg-slate-300 h-screen flex justify-center">
             <div className="flex flex-col justify-center">
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                 <Heading label={"Sign up"}/>
                 <SubHeading label={"Enter the information to create your account"} />
-                <Inputbox placeholder={"John"} label={"First Name"}/>
-                <Inputbox placeholder={"Doe"} label={"Last Name"}/>
-                <Inputbox placeholder={"abcd@gmail.com"} label={"Email"}/>
-                <Inputbox placeholder={"12345678"} label={"Password"}/>
+                <Inputbox onChange={e=>{setFirstName(e.target.value)}} placeholder={"John"} label={"First Name"}/>
+                <Inputbox onChange={e=>{setLastName(e.target.value)}} placeholder={"Doe"} label={"Last Name"}/>
+                <Inputbox onChange={e=>{setUserName(e.target.value)}} placeholder={"abcd@gmail.com"} label={"Email"}/>
+                <Inputbox onChange={e=>{setpassword(e.target.value)}} placeholder={"12345678"} label={"Password"}/>
                 <div className="pt-4">
-                    <Button label={"Sign up"}/>
+                    <Button onClick={ async (req,res)=>{
+                       await axios.post("http://localhost:3001/api/v1/user/signup", {
+                            username: username,
+                            firstName: firstName,
+                            lastName: lastName,
+                            password: password
+
+                        })
+                        res.status(200).json({
+                            msg:"Signup successfull"
+                        })
+                    }} label={"Sign up"}/>
                 </div>
                 <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"}/>
                 </div>
